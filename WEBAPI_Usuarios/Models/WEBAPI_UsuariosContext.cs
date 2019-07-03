48f55565bf9a0d7fa7823fd49076a25a313bdfb4
Reginaldo L. Caumo <rlcaumo@gmail.com>
2019-07-03 07:04:59 -0300

    Add project files.

---
+++ WEBAPI_Usuarios/Models/WEBAPI_UsuariosContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WEBAPI_Usuarios.Entities;
+
+namespace WEBAPI_Usuarios.Models
+{
+    public class WEBAPI_UsuariosContext : DbContext
+    {
+        public WEBAPI_UsuariosContext() : base("name=WEBAPI_UsuariosContext")
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public DbSet<DetalheUsuario> Detalhe_Usuarios { get; set; }
+    }
+}
